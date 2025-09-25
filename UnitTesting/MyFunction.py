dc6fe5127209671a1539df366ef71e5ce0342ec3
tasliima <181723853+Tasliima@users.noreply.github.com>
2025-09-25 15:15:12 +0100

    My first test case using incrementals of one

---
+++ UnitTesting/MyFunction.py
@@ -0,0 +1,3 @@
+# a minimal function to add two numbers
+def incrementals(a):
+    return a + 1
---
+++ UnitTesting/TestOne.py
@@ -0,0 +1,20 @@
+import unittest
+unittest.main()
+from MyFunction import incredmentals
+
+# define the unit tests
+class test_one(unittest.TestCase):
+    def test_incrementals(self):
+        
+        # test adding integers
+        self.assertEqual(my_incrementals(2), 3)
+
+        # test adding negative integers
+        self.assertEqual(my_incrementals(-6), -5)
+
+        # test adding floats
+        self.assertEqual(my_incrementals(3.1), 4.1)
+
+# run the tests
+if __name__ == "__main__":
+    unittest.main()
