55bf56e609c717d3668692226e61a8c3ec5fcfb3
tasliima <181723853+Tasliima@users.noreply.github.com>
2025-09-29 11:52:19 +0100

    Added comments for documentation purposes

--- Compound Interest Calculator.py
+++ Compound Interest Calculator.py
@@ -3,11 +3,11 @@ import math
 def InterestCal(Savings, AnnualInterestRate, Years):
     for i in range(0, Years): # range default 0 to number of years
         Amount = Savings * math.pow((1 + AnnualInterestRate/100), i)
-        print("For Year", i+1," you will have a balance of £", round(Amount, 2))
+        print("For Year", i+1," you will have a balance of £", round(Amount, 2)) # prints the balance for each year, to 2 decimal places
     return Amount
 
 Savings = float(input("input your savings: £"))
 AnnualInterestRate = float(input("input your interest rate:"))
 Years = int(input("input your overall years:"))
-print ("After", Years, "years, you will have: £", round(InterestCal(Savings, AnnualInterestRate, Years)))
+print ("After", Years, "years, you will have: £", round(InterestCal(Savings, AnnualInterestRate, Years))) # prints the final balance to 2 decimal places
 InterestCal(Savings, AnnualInterestRate, Years)
