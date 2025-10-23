ef32aeb59e704800a092f0796a2184368492211f
tasliima <181723853+Tasliima@users.noreply.github.com>
2025-10-23 15:32:43 +0100

    began trying to plot a histogram

--- us_elections.py
+++ us_elections.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 
-#Load election data
+# Load election data
 election_data =pd.read_csv('US-2016-primary.csv',delimiter=';')
 print(election_data.head())
+
+# Create a histogram of the votes for each candidate by state
+election_data.hist(column='Votes', by='State', figsize=(15.10), bins=20)
+plt.plot.show()
