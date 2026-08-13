bf20522e0b9bb45c1081c4d9d9c04edc83f6c8c1
tasliima <181723853+Tasliima@users.noreply.github.com>
2026-08-13 20:17:10 +0100

    Fit first order polynomial to training data

--- life_expectancy.py
+++ life_expectancy.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the life expectancy data
@@ -15,3 +16,11 @@ testing = world[world["Year"] > 2013]
 
 print("Training data:", training["Year"].min(), "to", training["Year"].max())
 print("Testing data:", testing["Year"].min(), "to", testing["Year"].max())
+
+# Fit a first-order polynomial (straight line)
+x_train = training["Year"].values
+y_train = training["Life expectancy"].values
+
+coefficients = np.polyfit(x_train, y_train, 1)
+
+print("Order 1 coefficients:", coefficients)
